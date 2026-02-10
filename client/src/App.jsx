b7194a0991bf770e4829cdb8ee49fe9404aec3bc
Bhavishya Sharma <bhavishaya32@gmail.com>
2026-02-10 14:21:28 +0530

    client setup

---
+++ client/src/App.jsx
@@ -0,0 +1,11 @@
+import { Button } from "./components/ui/button.jsx";
+const App  = ()=>{
+  return (
+    <>
+    <h1>Chaliye shuru karte hai.</h1>
+    <Button className="bg-black text-white">press it </Button>
+    </>
+  )
+}
+
+export default App;
