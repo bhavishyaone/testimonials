5c5c2034c11a2bfac719b043bfe1146ff83e43cc
Bhavishya Sharma <bhavishaya32@gmail.com>
2026-02-11 11:52:10 +0530

    updated the workspaceSchema

--- server/src/models/Workspace.js
+++ server/src/models/Workspace.js
@@ -1,11 +1,19 @@
 import mongoose from "mongoose";
 
+const workspaceSchema = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: true,
+      trim: true
+    },
+    owner: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+      required: true
+    }
+  },
+  { timestamps: true }
+);
 
-const workspaceSchema = new mongoose.Schema({
-    name: String,
-    owner: { type: mongoose.Schema.Types.ObjectId, ref: "User" }
-},
-    {timestamps:true}
-)
-
-export default mongoose.model("Workspace",workspaceSchema);
+export default mongoose.model("Workspace", workspaceSchema);
