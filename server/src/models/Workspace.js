8c3a9c01796a05ef9ce1845f651b4af69c7d40c7
Bhavishya Sharma <bhavishaya32@gmail.com>
2026-02-17 17:49:23 +0530

    updated workspace schema and deleted collection schema

--- server/src/models/Workspace.js
+++ server/src/models/Workspace.js
@@ -1,19 +1,73 @@
 import mongoose from "mongoose";
 
+
 const workspaceSchema = new mongoose.Schema(
   {
-    name: {
+    name:{
+      type:String,
+      required:true,
+      trim:true
+    },
+
+    logo: {
+      type: String
+    },
+
+    headerTitle: {
+      type: String,
+      required: true
+    },
+
+    customMessage: {
       type: String,
       required: true,
-      trim: true
+      minlength: 30
     },
+
+    collectName: {
+      type: Boolean,
+      default: true
+    },
+
+    collectEmail: {
+      type: Boolean,
+      default: true
+    },
+
+    collectionType: {
+      type: String,
+      enum: ["text", "video", "both"],
+      default: "both"
+    },
+
+    allowStarRating: {
+      type: Boolean,
+      default: false
+    },
+
+    theme: {
+      type: String,
+      enum: ["light", "dark"],
+      default: "light"
+    },
+
+    slug: {
+      type: String,
+      unique: true,
+      required: true
+    },
+
     owner: {
       type: mongoose.Schema.Types.ObjectId,
       ref: "User",
       required: true
     }
   },
+  
   { timestamps: true }
 );
+
+workspaceSchema.index({ slug: 1 }, { unique: true });
+workspaceSchema.index({ owner: 1 });
 
 export default mongoose.model("Workspace", workspaceSchema);
